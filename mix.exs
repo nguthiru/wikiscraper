8a164cfe0619c4973212e044d90e4739d7a271f1
nguthiru <eddiendiritu@gmail.com>
2024-06-13 21:02:57 +0300

    chore: changed base elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Wikiscraper.MixProject do
     [
       app: :wikiscraper,
       version: "0.1.0",
-      elixir: "~> 1.16",
+      elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
